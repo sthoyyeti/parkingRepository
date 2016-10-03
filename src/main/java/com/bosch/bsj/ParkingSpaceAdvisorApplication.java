0bfedb0346d07a8dc6a27988ea00eb7121fa0add
sri9cob <sri9cob@BMHE1069887>
2016-10-03 11:19:38 +0530

    ParkingSpaceAdvisor Application

---
+++ src/main/java/com/bosch/bsj/ParkingSpaceAdvisorApplication.java
@@ -0,0 +1,12 @@
+package com.bosch.bsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpaceAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSpaceAdvisorApplication.class, args);
+	}
+}
